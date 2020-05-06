a028a33068a7dc7d00fb1e7385f95cd96aa4f466
Michael Beaton <beatonma@gmail.com>
2020-05-06 20:48:38 +0100

    Updated to Kotlin 1.4 preview. Add coroutines and HTML DSL

--- buildSrc/src/main/kotlin/Dependency.kt
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -1,7 +1,16 @@
+private fun dependency(group: String, artifact: String, version: String): String {
+    return "$group:$artifact:$version"
+}
+
+private fun kotlinx(artifact: String, version: String = Version.KOTLIN) =
+    dependency(group = "org.jetbrains.kotlinx", artifact = artifact, version = version)
+
+
 object Version {
-    const val KOTLIN = "1.3.72"
+    const val KOTLIN = "1.4-M1"
 }
 
 object Dependency {
-
+    val COROUTINES_CORE_JS = kotlinx("kotlinx-coroutines-core-js", version = "1.3.5-native-mt-1.4-M1")
+    val KOTLIN_HTML_JS = kotlinx("kotlinx-html-js", version = "0.6.12")
 }

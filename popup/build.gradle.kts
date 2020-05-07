e8b4abbea5eb8e90a1383292d296b70fd3409747
Michael Beaton <beatonma@gmail.com>
2020-05-07 19:57:05 +0100

    Add Kotlin HTML DSL and coroutines

--- popup/build.gradle.kts
+++ popup/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
+    implementation(Dependency.COROUTINES_CORE_JS)
+    implementation(Dependency.KOTLIN_HTML_JS)
     implementation(project(":chrome-platform"))
     implementation(project(":common"))
     implementation(project(":dom"))

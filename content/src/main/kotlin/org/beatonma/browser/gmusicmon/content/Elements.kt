6623bcbd8ce39bcf26ffb6f850a8bdf34702175f
Michael Beaton <beatonma@gmail.com>
2020-05-10 19:52:41 +0100

    HTML class/id constants

---
+++ content/src/main/kotlin/org/beatonma/browser/gmusicmon/content/Elements.kt
@@ -0,0 +1,17 @@
+package org.beatonma.browser.gmusicmon.content
+
+object Elements {
+    private const val PREFIX = "gmusic"
+
+    const val MAIN = "$PREFIX-update"
+    const val MESSAGE = "$PREFIX-update-message"
+    const val HEADER = "$PREFIX-header"
+    const val COMPARISON = "$PREFIX-comparison"
+    const val COLUMN = "$PREFIX-column"
+    const val ARTIST = "$PREFIX-artist"
+    const val ADDED = "$PREFIX-accent-added"
+    const val REMOVED = "$PREFIX-accent-removed"
+
+    const val ACTIONS = "$PREFIX-actions"
+    const val CLOSE_BUTTON = "$PREFIX-action-close"
+}

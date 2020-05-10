c4303504166f4c9312c44e91cbab02763a3b17dd
Michael Beaton <beatonma@gmail.com>
2020-05-10 22:02:43 +0100

    New styles and class names for diff lists and list items.

--- content/src/main/kotlin/org/beatonma/browser/gmusicmon/content/Elements.kt
+++ content/src/main/kotlin/org/beatonma/browser/gmusicmon/content/Elements.kt
@@ -6,9 +6,18 @@ object Elements {
     const val MAIN = "$PREFIX-update"
     const val MESSAGE = "$PREFIX-update-message"
     const val HEADER = "$PREFIX-header"
+
     const val COMPARISON = "$PREFIX-comparison"
     const val COLUMN = "$PREFIX-column"
+    const val COLUMN_CONTENT = "$PREFIX-column-content"
     const val ARTIST = "$PREFIX-artist"
+    const val ALBUM = "$PREFIX-album"
+    const val ALBUM_NAME = "$PREFIX-album-name"
+    const val ALBUM_ARTIST = "$PREFIX-album-artist"
+
+    const val ITEM = "$PREFIX-item"
+    const val INLINE = "$PREFIX-inline"
+
     const val ADDED = "$PREFIX-accent-added"
     const val REMOVED = "$PREFIX-accent-removed"
 

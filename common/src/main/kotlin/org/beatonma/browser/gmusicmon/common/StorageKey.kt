a5dd21272fa554dd316037d202316c52a8fc3d45
Michael Beaton <beatonma@gmail.com>
2020-05-10 21:07:33 +0100

    Added ALBUM_LIST

--- common/src/main/kotlin/org/beatonma/browser/gmusicmon/common/StorageKey.kt
+++ common/src/main/kotlin/org/beatonma/browser/gmusicmon/common/StorageKey.kt
@@ -2,5 +2,6 @@ package org.beatonma.browser.gmusicmon.common
 
 object StorageKey {
     const val ARTIST_LIST = "artist_list"
+    const val ALBUM_LIST = "album_list"
     const val LAST_UPDATED = "last_updated"
 }

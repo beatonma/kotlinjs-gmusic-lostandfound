43d0bc2d4fdcd8aca3f8780ca69044481d8ab898
Michael Beaton <beatonma@gmail.com>
2020-05-10 21:09:02 +0100

    Generalised UI building

---
+++ content/src/main/kotlin/org/beatonma/browser/gmusicmon/content/BaseUi.kt
@@ -0,0 +1,123 @@
+package org.beatonma.browser.gmusicmon.content
+
+import kotlinx.html.*
+import kotlinx.html.dom.append
+import kotlinx.html.js.onClickFunction
+import org.beatonma.browser.gmusicmon.common.browser
+import org.beatonma.browser.gmusicmon.common.getResourceUrl
+import org.w3c.dom.Element
+import org.w3c.dom.HTMLElement
+import kotlin.browser.document
+import kotlin.dom.clear
+import kotlin.dom.createElement
+
+interface BaseUi<T> {
+    val columnTitles: ColumnTitles
+    val updateMessages: UpdateMessages
+
+    fun close() {
+        document.getElementById(Elements.MAIN)?.remove()
+    }
+
+    fun getContainer(): Element {
+        return document.getElementById(Elements.MAIN)?.also { it.clear() }
+            ?: document.createElement("div") {
+                id = Elements.MAIN
+                classList.add(Elements.MAIN)
+            }.also { document.body?.append(it) }
+    }
+
+    private fun showNoChanges() {
+        getContainer().append {
+            div(Elements.MESSAGE) {
+                id = Elements.MESSAGE
+                +"No changes :)"
+            }
+        }
+    }
+
+    fun showComparison(
+        added: Set<T>,
+        removed: Set<T>
+    ) {
+        if (added.isEmpty() && removed.isEmpty()) {
+            showNoChanges()
+            return
+        }
+        getContainer().append {
+            div(Elements.HEADER) {
+                div { }
+                div(Elements.ACTIONS) {
+                    img(alt = "Close", classes = Elements.CLOSE_BUTTON, src = browser.getResourceUrl("ic_close.svg")) {
+                        onClickFunction = { close() }
+                    }
+                }
+            }
+            div(Elements.COMPARISON) {
+                div(Elements.COLUMN) {
+                    h1 { +columnTitles.added }
+                    div(Elements.COLUMN_CONTENT) {
+                        addedList(added)
+                    }
+                }
+                div(Elements.COLUMN) {
+                    h1 { +columnTitles.removed }
+                    div(Elements.COLUMN_CONTENT) {
+                        removedList(removed)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun TagConsumer<HTMLElement>.addedList(added: Set<T>) =
+        added.map { item -> wrappedListItem(item, Elements.ADDED) }
+
+    private fun TagConsumer<HTMLElement>.removedList(removed: Set<T>) =
+        removed.map { item -> wrappedListItem(item, Elements.REMOVED) }
+
+    private fun TagConsumer<HTMLElement>.wrappedListItem(item: T, className: String): HTMLElement =
+        div(Elements.ITEM) {
+            span(className) {
+                when (className) {
+                    Elements.ADDED -> +"+"
+                    Elements.REMOVED -> +"-"
+                }
+            }
+            listItem(item)
+        }
+
+    fun TagConsumer<HTMLElement>.listItem(item: T): HTMLElement
+
+    private fun TagConsumer<HTMLElement>.artistListItem(name: ArtistName, className: String): HTMLElement =
+        div(Elements.ARTIST) {
+            span(className) {
+                when (className) {
+                    Elements.ADDED -> +"+"
+                    Elements.REMOVED -> +"-"
+                }
+            }
+            span { +name }
+        }
+
+    fun showUpdateMessage(itemCount: Int, updateComplete: Boolean) {
+        getContainer().append {
+            div(Elements.MESSAGE) {
+                id = Elements.MESSAGE
+                +updateMessages.getMessage(itemCount, updateComplete)
+            }
+        }
+    }
+
+}
+
+data class ColumnTitles(val added: String, val removed: String)
+interface UpdateMessages {
+    fun updateCompleteMessage(itemCount: Int): String
+    fun updateOngoingMessage(itemCount: Int): String
+
+    fun getMessage(itemCount: Int, updateComplete: Boolean) = when (updateComplete) {
+        true -> updateCompleteMessage(itemCount)
+        false -> updateOngoingMessage(itemCount)
+    }
+}
--- content/src/main/kotlin/org/beatonma/browser/gmusicmon/content/artists/ArtistUi.kt
+++ content/src/main/kotlin/org/beatonma/browser/gmusicmon/content/artists/ArtistUi.kt
@@ -1,96 +1,21 @@
 package org.beatonma.browser.gmusicmon.content.artists
 
 import kotlinx.html.*
-import kotlinx.html.dom.append
-import kotlinx.html.js.onClickFunction
-import org.beatonma.browser.gmusicmon.common.browser
-import org.beatonma.browser.gmusicmon.common.getResourceUrl
-import org.beatonma.browser.gmusicmon.content.Elements
-import org.w3c.dom.Element
+import org.beatonma.browser.gmusicmon.content.*
 import org.w3c.dom.HTMLElement
-import kotlin.browser.document
-import kotlin.dom.clear
-import kotlin.dom.createElement
 
-object ArtistUi {
-    private fun close() {
-        document.getElementById(Elements.MAIN)?.remove()
-    }
 
-    private fun getContainer(): Element {
-        return document.getElementById(Elements.MAIN)?.also { it.clear() }
-            ?: document.createElement("div") {
-                id = Elements.MAIN
-                classList.add(Elements.MAIN)
-            }.also { document.body?.append(it) }
-    }
+object ArtistUi : BaseUi<ArtistName> {
+    override val columnTitles: ColumnTitles
+        get() = ColumnTitles("Added artists", "Removed artists")
+    override val updateMessages: UpdateMessages
+        get() = object: UpdateMessages {
+            override fun updateCompleteMessage(itemCount: Int) =
+                "Update complete! Found $itemCount artists."
 
-    private fun showNoChanges() {
-        getContainer().append {
-            div(Elements.MESSAGE) {
-                id = Elements.MESSAGE
-                +"No changes :)"
-            }
-        }
-    }
-
-    fun showComparison(added: Artists, removed: Artists) {
-        if (added.isEmpty() && removed.isEmpty()) {
-            showNoChanges()
-            return
-        }
-        getContainer().append {
-            div(Elements.HEADER) {
-                div { }
-                div(Elements.ACTIONS) {
-                    img(alt = "Close", classes = Elements.CLOSE_BUTTON, src = browser.getResourceUrl("ic_close.svg")) {
-                        onClickFunction = { close() }
-                    }
-                }
-            }
-            div(Elements.COMPARISON) {
-                div(Elements.COLUMN) {
-                    h1 { +"Added artists" }
-                    artistList(added, Elements.ADDED)
-                }
-                div(Elements.COLUMN) {
-                    h1 { +"Removed artists" }
-                    artistList(removed, Elements.REMOVED)
-                }
-            }
-        }
-    }
-
-    private fun TagConsumer<HTMLElement>.artistList(artists: Artists, className: String): List<HTMLElement> =
-        artists.map { name -> artistListItem(name, className) }
-
-    private fun TagConsumer<HTMLElement>.artistListItem(name: ArtistName, className: String): HTMLElement =
-        div(Elements.ARTIST) {
-            span(className) {
-                when (className) {
-                    Elements.ADDED -> +"+"
-                    Elements.REMOVED -> +"-"
-                }
-            }
-            span { +name }
+            override fun updateOngoingMessage(itemCount: Int) =
+                "$itemCount artists discovered..."
         }
 
-    fun showUpdateMessage(data: ArtistUiData) {
-        val message = when (data.updateComplete) {
-            true -> "Update complete! Found ${data.artistCount} artists."
-            false -> "${data.artistCount} artists discovered..."
-        }
-        getContainer().append {
-            div(Elements.MESSAGE) {
-                id = Elements.MESSAGE
-                +message
-            }
-        }
-    }
+    override fun TagConsumer<HTMLElement>.listItem(item: ArtistName): HTMLElement = span { +item }
 }
-
-
-data class ArtistUiData(
-    var updateComplete: Boolean = false,
-    var artistCount: Int = 0
-)
